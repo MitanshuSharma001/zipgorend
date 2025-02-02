aad20fa0cdfbc4a0691422fc58c5107d79a204d1
MitanshuSharma001 <157119715+MitanshuSharma001@users.noreply.github.com>
2025-02-02 19:30:06 +0530

    Update date.js

--- date.js
+++ date.js
@@ -1,8 +1,9 @@
 function date() {
         let date = new Date()
-        let hours = String(date.getHours()).padStart(2,'0')
-        let minutes = String(date.getMinutes()).padStart(2,'0')
-        let seconds = String(date.getSeconds()).padStart(2,'0')
+        let istDate = date.toLocaleString("en-IN", { timeZone: "Asia/Kolkata" });
+        let hours = String(new Date(istDate).getHours()).padStart(2,'0')
+        let minutes = String(new Date(istDate).getMinutes()).padStart(2,'0')
+        let seconds = String(new Date(istDate).getSeconds()).padStart(2,'0')
         return `${hours}${minutes}${seconds}`
 }
 module.exports = date
