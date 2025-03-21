1e98bb9474209967896a3b8dfc2ee78d2565b9d9
MitanshuSharma001 <157119715+MitanshuSharma001@users.noreply.github.com>
2025-03-21 15:02:58 +0530

    Update socket.js

--- services/socket.js
+++ services/socket.js
@@ -1,7 +1,7 @@
 const {io} = require('socket.io-client')
 
 
-const socket = io('https://h62jskdt-5001.inc1.devtunnels.ms')
+const socket = io('https://njqd2w3x-5001.inc1.devtunnels.ms')
 socket.on('connect',()=>{
     console.log(`SocketId: ${socket.id}`); 
 })
